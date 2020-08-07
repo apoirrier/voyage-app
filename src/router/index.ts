1407db2361834f68146b333bd116729d4d886a46
Alexandre Poirrier <alexandre.poirrier@polytechnique.org>
2020-08-07 11:26:23 +0200

    Added start of PoI page with navigation bar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import PoI from '../views/PoI.vue'
 
 Vue.use(VueRouter)
 
   const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'PoI',
+    component: PoI
   },
-  {
+  /*{
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  }*/
 ]
 
 const router = new VueRouter({
---
+++ src/views/PoI.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class=navigation> <router-link :to="parent.url"> {{ parent.name }} </router-link> > <router-link :to="url"> {{ name }} </router-link> </div>
+        <div class=backMap> <router-link to="/"> Retour à la carte </router-link></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PoI",
+    data() {
+        return {
+            parent: {
+                name: "Suisse",
+                url: "/schweiss"
+            },
+            name: "Schauenstein Schloss",
+            localUrl: "/schauenstein"
+        }
+    },
+    computed: {
+        url() {
+            return this.parent.url + this.localUrl
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.navigation {
+    justify-content: left;
+    float: left;
+    font-size: 10px;
+    color: #111111;
+}
+
+.backMap {
+    justify-content: right;
+    float: right;
+    font-size: 10px;
+    color: #111111;
+}
+</style>
