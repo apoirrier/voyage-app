8e7bc9710f0ab387aebbf8c7774a31a504d41a4b
Alexandre Poirrier <alexandre.poirrier@polytechnique.org>
2020-08-07 15:38:52 +0200

    Added main description space

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import rate from 'vue-rate'
+import 'vue-rate/dist/vue-rate.css'
 
 Vue.config.productionTip = false
+Vue.use(rate)
 
 new Vue({
   router,
