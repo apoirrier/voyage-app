81215f826916bed6cd7ee3d61173416e610b6668
Alexandre Poirrier <alexandre.poirrier@polytechnique.org>
2023-01-31 12:20:16 +0100

    Upgrade vue sanitize to vuz-3-sanitize

--- src/additional_libs.d.ts
+++ src/additional_libs.d.ts
@@ -1,6 +1,6 @@
 declare module 'vue-rate';
 declare module 'parse';
 declare module 'mapbox-gl';
-declare module 'vue-sanitize';
+declare module 'vue-3-sanitize';
 declare module 'vue-clipboard2';
 declare module 'axios';
